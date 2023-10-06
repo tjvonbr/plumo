67f4aed000916382b2ce9155de5b0c6d292ea2c3
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-10-05 17:18:51 -0700

    auth UI

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,38 @@
+import LoginForm from "@/components/LoginForm";
+import RegisterForm from "@/components/RegisterForm";
+import Link from "next/link";
+
+export default function Register() {
+  return (
+    <div className="grid grid-cols-2">
+      <div className="h-screen bg-slate-100"></div>
+      <div className="min-h-screen w-1/2 m-auto flex flex-col justify-center items-center space-y-6">
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl text-black font-bold">
+            Welcome to Kaleidoscopik!
+          </h1>
+          <p className="text-sm text-slate-500 text-center">
+            Enter a few details about yourself below
+          </p>
+        </div>
+        <RegisterForm />
+        <div className="w-full relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t" />
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-white px-2 text-slate-500">
+              Or continue with
+            </span>
+          </div>
+        </div>
+        <button className="h-10 w-full bg-white hover:bg-slate-100 transition-colors border border-slate-300 rounded-md text-sm text-black font-medium">
+          Google
+        </button>
+        <Link className="text-sm text-slate-500 hover:underline" href="/login">
+          Already have an account? Sign in here
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,43 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname, useSelectedLayoutSegment } from "next/navigation";
 
 export function Header() {
+  const segment = useSelectedLayoutSegment();
+  const isAuth = segment === "(auth)";
+
   return (
-    <header className="h-20 w-full p-10 flex items-center space-x-10 border-b border-gray-200 text-black">
-      <span className="text-lg font-black">🎨 Kaleidoscopik</span>
-      <nav className="flex items-center space-x-4">
-        <Link
-          className="text-black text-sm font-medium hover:text-gray-500 transition-colors"
-          href="/create"
-        >
-          Create
-        </Link>
-        <Link
-          className="text-black text-sm font-medium hover:text-gray-500 transition-colors"
-          href="/"
-        >
-          Explore
-        </Link>
-      </nav>
+    <header
+      className={cn(
+        "absolute top-0 left-0 h-20 w-full p-10 flex flex-row justify-between items-center border-b border-gray-200 text-black",
+        isAuth ? "hidden" : ""
+      )}
+    >
+      <div className="flex items-center space-x-10">
+        <span className="text-lg font-black">🎨 Kaleidoscopik</span>
+        <nav className="flex items-center space-x-4">
+          <Link
+            className="text-black text-sm font-medium hover:text-gray-500 transition-colors"
+            href="/explore"
+          >
+            Explore
+          </Link>
+          <Link
+            className="text-black text-sm font-medium hover:text-gray-500 transition-colors"
+            href="/create"
+          >
+            Create
+          </Link>
+        </nav>
+      </div>
+      <Link
+        href="/login"
+        className="h-9 px-4 flex justify-center items-center bg-slate-100 hover:bg-slate-200 transition-colors border border-slate-300 rounded-md text-black text-sm"
+      >
+        Login
+      </Link>
     </header>
   );
 }
